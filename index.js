229310fe7476c14fbc7e5a53de5a779a667c85cd
manichandanreddy14 <suddalamanichandanreddy62@gmail.com>
2023-12-12 19:37:56 +0530

    pratic

---
+++ index.js
@@ -0,0 +1,21 @@
+//console.log("1");
+//let arr=[1,2,3,4,5];
+// Array.prototype.test=function(){
+//   for(let i=0; i<arr.length;i++){
+//     return arr[i]+1;
+//   }
+// }
+
+// console.log(arr.test());
+// let obj1={
+//     name:"mani",
+//     age:22,
+//     btech(){
+// console.log(this.name+this.clg);
+//     }
+// }
+// obj2={
+//    clg:"sree chaitanya"
+//  }
+//  obj1.__proto__=obj2;
+//  obj1.btech();
